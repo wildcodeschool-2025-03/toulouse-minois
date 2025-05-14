274398875d87825e3b16b43c980cc6afb967e724
CAYUELA Annick <annickcayuela.ac@gmail.com>
2025-05-14 17:17:35 +0200

    feat(Artdetail): Add Artdetails in page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,42 +5,41 @@ import type { Record } from "./types/HarvardType.tsx";
 import "./stylesheets/normalize.css";
 import "./stylesheets/App.css";
 import "./stylesheets/filter.css";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useQuery } from "@tanstack/react-query";
 import redaxios from "redaxios";
 
 const subject = "portrait";
 const classificationA = "Paintings";
 const classificationB = "Photographs";
 const packageSize = 100;
+const page = 1;
 
-async function fetchHarvardAPI({ pageParam = 1 }) {
-  const urlHarvard = `https://api.harvardartmuseums.org/object?apikey=${import.meta.env.VITE_REACT_APP_HARVARD_MUSEUM_API}&q=classification=${classificationA}&q=classification=${classificationB}&keyword=${subject}&size=${packageSize}&page=${pageParam}`;
+async function fetchHarvardAPI() {
+  const urlHarvard = `https://api.harvardartmuseums.org/object?apikey=${import.meta.env.VITE_REACT_APP_HARVARD_MUSEUM_API}&q=classification=${classificationA}&q=classification=${classificationB}&keyword=${subject}&size=${packageSize}&page=${page}`;
   const { data } = await redaxios.get(urlHarvard);
-    const validArt = data.records.filter((record: Record) => record.primaryimageurl && record.primaryimageurl.trim() !== "");
-  return { records: validArt, nextPage: pageParam + 1, hasMore: data.info.next !== null };
+  const validArt = data.records.filter(
+    (record: Record) => record.primaryimageurl,
+  );
+  return { ...data, records: validArt };
 }
 
 function App() {
   const [dailyPortrait, setDailyPortrait] = useState<Record>({} as Record);
 
-  const {
-    data,
-    isLoading,
-    isError,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage
-  } = useInfiniteQuery({
-    queryKey: ['harvardArt', subject, classificationA, classificationB, packageSize],
+  const { data: artHarvardData } = useQuery({
+    queryKey: [
+      "harvardArt",
+      subject,
+      classificationA,
+      classificationB,
+      packageSize,
+      page,
+    ],
     queryFn: fetchHarvardAPI,
-    getNextPageParam: (lastPage) => (lastPage.hasMore ? lastPage.nextPage : undefined),
-    initialPageParam: 1,
     staleTime: 15 * 60 * 1000,
   });
 
-  const art = data?.pages.flatMap((page) => page.records) || [];
+  const art = artHarvardData?.records || [];
 
   const artMemo = useMemo(() => {
     return art;
@@ -55,42 +54,41 @@ function App() {
 
   useEffect(() => {
     selectRandomPortrait();
-    const interval = setInterval(
-        () => {
-          selectRandomPortrait();
-        },
-        15 * 60000,
-    );
+    const interval = setInterval(() => {
+      selectRandomPortrait();
+    }, 15 * 60000);
 
     return () => clearInterval(interval);
   }, [selectRandomPortrait]);
 
-    return (
-        <HarvardMuseumAPIContext
-            value={{
-                dailyPortrait,
-                artMemo,
-                art,
-                checkbox: [],
-                hasNextPage,
-                isFetchingNextPage,
-                fetchNextPage,
-            }}
-        >
-            <nav>
-                <p>Minois</p>
-                <Link to="/">Home</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/about">About</Link>
-            </nav>
-            <main>
-                {isLoading ? <p>Minute Papillon</p> : null}
-                {isError ? <p>Flute alors ! {error?.message}</p> : null}
-                {!isLoading && !isError && <Outlet />}
-            </main>
-            <ReactQueryDevtools initialIsOpen={false} />
-        </HarvardMuseumAPIContext>
-    );
+  const loadArtwork = (id: string) => {
+    const artworkId = Number.parseInt(id, 10);
+
+    if (artMemo) {
+      const artwork = artMemo.find((art: Record) => art.objectid === artworkId);
+
+      if (artwork) {
+        return { artwork };
+      }
+    }
+    return { error: "Les données de la galerie n'ont pas été chargées." };
+  };
+
+  return (
+    <HarvardMuseumAPIContext.Provider
+      value={{ art, dailyPortrait, artMemo, loadArtwork, checkbox: [] }}
+    >
+      <nav>
+        <p>Minois</p>
+        <Link to="/">Home</Link>
+        <Link to="/Gallery">Gallery</Link>
+        <Link to="/About">About</Link>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </HarvardMuseumAPIContext.Provider>
+  );
 }
 
 export default App;
--- client/src/pages/ArtDetail.tsx
+++ client/src/pages/ArtDetail.tsx
@@ -20,9 +20,57 @@ function ArtDetail() {
 
   return (
     <div>
-      <h1>{artwork.title}</h1>
-      <p>{artwork.description || "Aucune description disponible."}</p>
       <img src={artwork.primaryimageurl} alt={artwork.title} />
+      <h1>{artwork.title || "Unknown title"}</h1>
+      <h2>
+        <strong>Work Id :</strong> {artwork.id || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Artist: </strong>
+        {artwork.people?.[0]?.displayname || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Culture: </strong>
+        {artwork.people?.[0]?.culture || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Style :</strong> {artwork.classification || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Period :</strong> {artwork.dated || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Accession year :</strong> {artwork.accessionyear || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Technical :</strong> {artwork.technique || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Medium :</strong> {artwork.medium || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Dimensions :</strong> {artwork.dimensions || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Colors: </strong>{" "}
+        <p>Color count: {artwork.colorcount || "Unknown"}</p>
+        <p>Hexadecimal color code: {artwork.colors?.[0]?.color || "Unknown"}</p>
+        <p>Spectrum color: {artwork.colors?.[0]?.spectrum || "Unknown"}</p>
+        <p>Tint color: {artwork.colors?.[0]?.hue || "Unknown"}</p>
+        <p>Percent color: {artwork.colors?.[0]?.percent || "Unknown"}</p>
+      </h2>
+      <h2>
+        <strong>Division :</strong> {artwork.division || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Credit line :</strong> {artwork.creditline || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Contact :</strong> {artwork.contact || "Unknown"}
+      </h2>
+      <h2>
+        <strong>Copyright :</strong> {artwork.copyright || "Unknown"}
+      </h2>
     </div>
   );
 }
